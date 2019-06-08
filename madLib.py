a9f21beba777896bc653b20ca12ef7b2dcb868c1
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-06-07 22:36:13 -0400

    Final product

---
+++ madLib.py
@@ -0,0 +1,13 @@
+print("Tell me an adjective, and click enter. ")
+adjective1 = input()
+print("Tell me a noun (plural), and click enter. ")
+noun1 = input()
+print("Tell me another noun, and click enter. ")
+noun2 = input()
+print("Tell me an another adjective, and click enter. ")
+adjective2 = input()
+
+print ("Octopi are " + adjective1)
+print (noun1 + " are uneducated swine")
+print ("The " + noun2 + " is circular and " + adjective2)
+print ("And so are you!")
